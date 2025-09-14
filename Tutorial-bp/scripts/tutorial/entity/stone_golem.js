169243cb1be094c3b7c9d6e3a9c8f98c34e3bbc2
KawEduh <167013315+KawEduh-dv@users.noreply.github.com>
2025-09-14 17:52:28 -0300

    addon completado, pelo menos jogavel ate agr...

--- Tutorial-bp/scripts/tutorial/entity/stone_golem.js
+++ Tutorial-bp/scripts/tutorial/entity/stone_golem.js
@@ -1,9 +1,47 @@
-import { world } from '@minecraft/server';
+// Importe BlockPermutation junto com os outros módulos
+import { world, system, BlockPermutation } from "@minecraft/server";
 
-world.afterEvents.playerInteractWithEntity.subscribe(event => {
-    const { player, target } = event; // quem clicou e a entidade
+world.afterEvents.itemUse.subscribe(event => {
+    const { itemStack, source } = event;
+    if (itemStack.typeId === "tutorial:ruby_pickaxe") {
+        const block = source.getBlockFromViewDirection()?.block
+        world.sendMessage(`${block.getTags()}`);
+    }
+});
 
-    if (target.typeId === 'tutorial:stone_golem') {
-        player.sendMessage(`Um Golem de Pedra diz: Olá, Mundo!`);
+
+world.afterEvents.entitySpawn.subscribe(event => {
+    const { entity } = event;
+    if (entity.typeId === "tutorial:stone_golem") {
+        // Envia uma mensagem para o chat
+        world.sendMessage(`§6[Golem de Pedra]:§r Olá, Mundo!`);
+    }
+});
+
+world.afterEvents.playerPlaceBlock.subscribe((event) => {
+    const { block, dimension } = event;
+
+    if (block.typeId === "minecraft:carved_pumpkin") {
+        const blockBelow = block.below();
+
+        if (blockBelow && blockBelow.typeId === "minecraft:stone") {
+            system.run(() => {
+                const blockLocation = block.location;
+
+                block.dimension.runCommand(`gamerule dotiledrops false`);
+                block.dimension.runCommand(`fill ${blockLocation.x} ${blockLocation.y} ${blockLocation.z} ${blockLocation.x} ${blockLocation.y-1} ${blockLocation.z} air 0 destroy`);
+                block.dimension.runCommand(`gamerule dotiledrops true`);
+
+                // block.setPermutation(BlockPermutation.resolve('minecraft:air'));
+
+                // blockBelow.setPermutation(BlockPermutation.resolve('minecraft:air'));
+
+                dimension.spawnEntity("tutorial:stone_golem", {
+                    x: blockLocation.x + 0.5,
+                    y: blockLocation.y - 1,
+                    z: blockLocation.z + 0.5,
+                });
+            });
+        }
     }
 });
